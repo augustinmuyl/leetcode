4e64e7a6fa8ad7aa10f4b7f59bb5d453aef5f0d0
Augustin Muyl <154108037+augustinmuyl@users.noreply.github.com>
2025-06-27 13:59:12 +0200

    Time: 14 ms (12.61%), Space: 24.3 MB (7.97%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,22 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         A = {}
-        B = [[] for _ in range(len(nums) + 1)]
+        B = [[] for i in range(len(nums) + 1)]
         C = []
+        
         for i in nums:
-            A.update({i: A.get(i, 0) + 1})
-        for i in A:
-            B[A.get(i)].append(i)
-        for i in range(len(B) - 1, 0, -1):
-            if B[i]:
-                if (len(C) >= k):
-                    break
-                for j in B[i]:
+            if i not in A:
+                A.update({i:1})
+            else:
+                A.update({i:A.get(i)+1})
+        
+        for key, val in A.items():
+            B[val].append(key)
+
+        for i in range(len(B)-1, 0, -1):
+            for j in B[i]:
+                if len(C) < k:
                     C.append(j)
+        
         return C
         
