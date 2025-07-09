a9cc3e3677f797db91ae5c0a66840fbb7402c257
Augustin Muyl <154108037+augustinmuyl@users.noreply.github.com>
2025-07-09 15:56:00 +0200

    Time: 1586 ms (8.34%), Space: 20.2 MB (93.35%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -8,8 +8,11 @@ class Solution:
                 val = nums[l] + nums[r] + nums[i]
                 if val == 0:
                     A.add(tuple([nums[i], nums[l], nums[r]]))
+                    l+=1
+                    r-=1
                 elif val < 0:
                     l+=1
                     continue
-                r-=1
+                else:
+                    r-=1
         return list(A)
