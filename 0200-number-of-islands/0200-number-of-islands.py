43b828598e8d76ad212cc2e3963e4d65e2a25b7a
Augustin Muyl <154108037+augustinmuyl@users.noreply.github.com>
2025-10-14 14:56:00 -0400

    Time: 252 ms (48.28%), Space: 19.8 MB (99.39%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        q = deque([grid[0][0]])
+        res = 0
+
+        def dfs(r, c):
+            if r >= len(grid) or c >= len(grid[r]) or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"
+
+            dfs(r, c + 1)
+            if c != 0: dfs(r, c - 1)
+            dfs(r + 1, c)
+            if r != 0: dfs(r - 1, c)
+
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                if grid[r][c] == "1":
+                    res += 1
+                    dfs(r, c)
+
+        return res
