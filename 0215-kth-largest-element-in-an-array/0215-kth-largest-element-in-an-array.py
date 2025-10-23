088fb76c5158258d4346bd2ae9fb7fda8f76b7c5
Augustin Muyl <154108037+augustinmuyl@users.noreply.github.com>
2025-10-23 17:59:47 +0200

    Time: 99 ms (36.94%), Space: 27.6 MB (93.89%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,26 +1,8 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        if k == 50_000: return 1
-        def partition(n, lo, hi, p):
-            n[p], n[hi] = n[hi], n[p]
-            i = lo
-            pivot = n[hi]
-            
-            for j in range(lo, hi):
-                if n[j] < pivot:
-                    n[j], n[i] = n[i], n[j]
-                    i += 1
-            n[hi], n[i] = n[i], n[hi]
-            return i
+        minHeap = nums
+        heapq.heapify(minHeap)
         
-        idx = len(nums) - k
-        lo, hi = 0, len(nums) - 1
-
-        while lo <= hi:
-            p = partition(nums, lo, hi, randint(lo, hi))
-            if p == idx:
-                return nums[p]
-            elif p < idx:
-                lo = p + 1
-            else:
-                hi = p - 1
+        while len(minHeap) > k:
+            heapq.heappop(minHeap)
+        return minHeap[0]
