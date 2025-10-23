a8cd3944cb2941cf0c014b0d0d627f6065960e85
Augustin Muyl <154108037+augustinmuyl@users.noreply.github.com>
2025-10-23 17:56:17 +0200

    Time: 8 ms (92.49%), Space: 23.5 MB (83.62%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -0,0 +1,14 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        if len(self.minHeap) < self.k:
+            heapq.heappush(self.minHeap, val)
+        elif val > self.minHeap[0]:
+            heapq.heapreplace(self.minHeap, val)
+        return self.minHeap[0]
